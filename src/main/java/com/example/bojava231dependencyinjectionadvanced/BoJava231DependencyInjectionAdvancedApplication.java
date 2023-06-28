8feb64ed82c8f2eb153ea6b93f2b982b4abb4269
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-28 09:51:07 +0200

    Setup project (spring initializer lombok - cli)

---
+++ src/main/java/com/example/bojava231dependencyinjectionadvanced/BoJava231DependencyInjectionAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.bojava231dependencyinjectionadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoJava231DependencyInjectionAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoJava231DependencyInjectionAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bojava231dependencyinjectionadvanced/BoJava231DependencyInjectionAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bojava231dependencyinjectionadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoJava231DependencyInjectionAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
